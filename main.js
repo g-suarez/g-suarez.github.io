9e32cac8c0723a1cbbaab57f4b0c9f848f237867
Gabriella Suarez <g.suarez@correo.unimet.edu.ve>
2022-02-12 18:10:52 -0400

    mentira ahora si sirve

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); 
-}
-
 var skilNames = ["Ingles", "Coreano","Frances", "Python","Java","HTML","CSS","JavaScript","Piano","Ukelele","Canto", "Guitarra"];
 var skillis = {};
 var skillsArray = [];
@@ -57,6 +42,10 @@ function playNote(key) {
     key.classList.remove('active')
   })
 }
+
+
+
+
 
 function validateForm()                                    
 { 
@@ -107,4 +96,32 @@ function validateForm()
    
     alert("Su solicitud ha sido enviada");
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500); 
+}
+
+var skilNames = ["Ingles", "Coreano","Frances", "Python","Java","HTML","CSS","JavaScript","Piano","Ukelele","Canto", "Guitarra"];
+var skillis = {};
+var skillsArray = [];
+for (let i = 0; i < 12; i++) {
+    skillis.id = (i + 1);
+    skillis.name = skilNames[i];
+    skillsArray.push({...skillis});
+}
+
+
+
+
 
